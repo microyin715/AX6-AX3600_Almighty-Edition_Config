a9a3c2823023e87b88fe15183a2adf010a926745
microyin715 <66552744+microyin715@users.noreply.github.com>
2022-01-27 13:04:36 +0800

    Update part1.sh

--- PhicommK3/lede/part1.sh
+++ PhicommK3/lede/part1.sh
@@ -17,7 +17,8 @@
 #
 
 # Alter default router IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+echo '修改路由器默认IP'
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #修改NTP设置
 #sed -i "s/'0.openwrt.pool.ntp.org'/'ntp1.aliyun.com'/g" package/base-files/files/bin/config_generate
