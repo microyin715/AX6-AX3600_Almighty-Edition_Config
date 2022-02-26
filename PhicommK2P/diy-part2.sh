d538523367f5576be1b15d8b0a20e706c43ff0dc
microyin715 <66552744+microyin715@users.noreply.github.com>
2022-02-26 20:54:05 +0800

    Update diy-part2.sh

--- PhicommK2P/diy-part2.sh
+++ PhicommK2P/diy-part2.sh
@@ -11,5 +11,10 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.158.1/g' package/base-files/files/bin/config_generate
 
+#添加smartdns
+#git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
+git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+
 # 去除默认主题
 sed -i 's/+luci-theme-bootstrap/ /g' feeds/luci/collections/luci/Makefile
