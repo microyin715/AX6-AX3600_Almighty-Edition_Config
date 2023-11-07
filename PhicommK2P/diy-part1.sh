8a46a7d4f1ab5de3db937062fd9613fb45bbdf0b
microyin715 <66552744+microyin715@users.noreply.github.com>
2023-11-07 15:27:05 +0800

    Update diy-part1.sh

--- PhicommK2P/diy-part1.sh
+++ PhicommK2P/diy-part1.sh
@@ -13,4 +13,4 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-# sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld' feeds.conf.default
