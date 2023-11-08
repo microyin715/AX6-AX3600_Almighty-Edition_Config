f3dba8c87bf73a298baa6a6ecf2ab33b05f3d4fe
microyin715 <66552744+microyin715@users.noreply.github.com>
2023-11-08 13:16:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,5 @@
 
 echo '添加Passwall软件源'
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall https://github.com/microyin715/small.git' >>feeds.conf.default
+sed -i '$a src-git small https://github.com/microyin715/small' feeds.conf.default
 echo '=========Add feed source OK!========='
