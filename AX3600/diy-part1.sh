2349691577eb1d028eb74492f1ce4c154e787c88
microyin715 <66552744+microyin715@users.noreply.github.com>
2023-11-08 13:16:49 +0800

    Update diy-part1.sh

--- AX3600/diy-part1.sh
+++ AX3600/diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git small https://github.com/microyin715/small' feeds.conf.default
